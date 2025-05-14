abde2504e4ecbdd1debfeb6f1938224f14054c53
Debian <paul@aiur.whc.lan>
2025-05-14 08:05:51 +0000

    added remove search command keybind

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,3 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>cc", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>cx", vim.cmd.nohlsearch)
